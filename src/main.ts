77c6e13edfdde52d3eb2cb4f297304ed5f76b26b
Abanoub Essam <20130130@fa-hists.edu.eg>
2020-10-05 16:34:09 +0200

    awake heroku

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+const { AwakeHeroku } = require('awake-heroku');
 
 async function bootstrap() {
   const logger: Logger = new Logger('bootstrap');
 
   const port = process.env.PORT || 3000
   const app = await NestFactory.create(AppModule);
+  AwakeHeroku.add({
+    url: "https://webrtc-server-api.herokuapp.com/"
+  })
   await app.listen(port);
   logger.log(`App Starts with Port ${port}`)
 }
